b25ef0d3280425708b2c7d89ee6903aae6f2e257
Gilles Lagrilliere <gilles.lagrilliere@codraft.be>
2025-01-24 13:56:17 +0100

    config file added

--- CodingTracker/CodingTracker/Program.cs
+++ CodingTracker/CodingTracker/Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿using System.Configuration;
+
+string connectionstring = ConfigurationManager.AppSettings.Get("Connectionstring") ?? throw new Exception("Key value pair doesn't exist in the config-file!");
